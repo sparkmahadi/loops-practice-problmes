254328f99cc7d66fc4138ed1a4f98df14602991a
sparkmahadi <mahadi.development@gmail.com>
2022-08-02 16:28:48 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,17 @@
+// while loop
+// var roastGiven=0;
+// while(roastGiven<7){
+//     console.log('Roast Den, Please!!');
+//     roastGiven++;
+// }
+
+// for loop
+// for(var roastGiven=0;roastGiven<7;roastGiven++){
+//     console.log('Roast Den Pleas!!');
+// }
+
+// simple version of for loop
+console.log('simple for loop');
+for(var i=0; i <7; i++){
+    console.log(i);
+}
