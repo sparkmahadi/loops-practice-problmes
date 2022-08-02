47a788fd2d3ba5c9b6262fbd468ee13ffec59e5c
sparkmahadi <mahadi.development@gmail.com>
2022-08-02 20:16:30 +0600

    added old files

---
+++ revision.js
@@ -0,0 +1,10 @@
+var bottle = 'water';
+var bottleColor='yellow';
+var sunglassPrice='120';
+var penColor='blue';
+var isExpensive=true;
+
+var tableItems=['bottle','pen','mouse','sunglass','paper'];
+var item3=tableItems[3];
+tableItems[4]='notebook';
+var penIndex=tableItems.indexOf('pen');
---
+++ while.js
@@ -0,0 +1,11 @@
+// if(condition){
+    
+// }
+
+var roastGiven=0;
+while(roastGiven<7){
+    console.log("roast den please!!")
+    // roastGiven+=1;
+    console.log(roastGiven);
+    roastGiven++;
+}
