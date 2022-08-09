44668da3b822859100084457a0196d0e176b2802
sparkmahadi <mahadi.development@gmail.com>
2022-08-09 15:07:11 +0600

    added new files

--- array-loop.js
+++ array-loop.js
@@ -1,17 +1,18 @@
-// for(var i=0; i<10; i++){
-//     console.log(i);
-// }
+/* for(var i = 0; i < 10; i++){
+    console.log(i);
+} */
 
-// target: display every emelents of an array
+// target: display every elements of an array
+var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
 
-// var numbers=[45,87,89,56,32,51,25, 188, 41, 25];
-// for(var i=0; i< numbers.length; i++){
+// for(var i = 0; i < numbers.length; i++){
 //     var number = numbers[i];
 //     console.log(number);
 // }
 
-var items =['bottle','mouse','sunglass','pen','notebook'];
-for(var i=0; i<items.length; i++){
-    var item= items[i];
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
     console.log(item);
 }
